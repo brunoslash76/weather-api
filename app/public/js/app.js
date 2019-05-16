53a4f5a696aa5769186d0aabc016f0d00e717b72
Bruno Russo <bruno.russo@evolutionit.com.br>
2019-05-15 21:28:11 -0300

    Feature: Current Location Current Time

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -8,6 +8,32 @@ const initHeaders = {
     mode: 'cors',
     cache: 'default',
 };
+const weekDays = [
+    "Domingo-Feira",
+    "Segunda-Feira",
+    "Terça-Feira",
+    "Quarta-Feira",
+    "Quinta-Feira",
+    "Sexta-Feira",
+    "Sábado-Feira",
+];
+const months = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+];
+const ONE_MINUTE = 60000;
+const ONE_DAY = 1440000;
+const WEATHER_UPDATE = 180000;
 let weatherData
 
 if ('geolocation' in navigator) {
@@ -18,7 +44,12 @@ if ('geolocation' in navigator) {
 
 async function init() {
     // getWeatherData();
-    getCurrentWeather()
+    getCurrentWeather();
+    displayCurrentTime();
+    displayCurrentDate();
+    updateDisplays(displayCurrentDate, ONE_DAY);
+    updateDisplays(displayCurrentTime, ONE_MINUTE);
+    updateDisplays(getCurrentWeather, WEATHER_UPDATE);
 }
 
 function getWeatherData() {
@@ -38,13 +69,8 @@ function getWeatherData() {
     );
 }
 
-
-function handleWeatherData(weatherData) {
-    
-}
-
 function getCurrentWeather() {
-    navigator.geolocation.getCurrentPosition( pos => {
+    navigator.geolocation.getCurrentPosition(pos => {
         const CALL = `${_SERVER_NAME}/weather?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=${_API_KEY}`;
         fetch(CALL, initHeaders)
             .then(data => data.json())
@@ -54,17 +80,56 @@ function getCurrentWeather() {
 }
 
 function displayCurrenteLocationData(data) {
-    console.log(data)
     const currentCity = document.querySelector('#current-city');
     const currentTemperature = document.querySelector('#current-temperature');
     currentCity.innerHTML = `${data.name}, ${data.sys.country}`;
-    currentTemperature.innerHTML = `${kelvinToCelcius(data.main.temp)}°`
+    currentTemperature.innerHTML = `${kelvinToCelcius(data.main.temp).toFixed(1, 10)}°`
+}
+
+
+// TIME FUNCTIONS 
+function getCurrentTime() {
+
+    const agora = Date.now();
+    const now = new Date(agora);
+    weekday = weekDays[now.getDay()];
+    month = months[now.getMonth()]
+
+    return {
+        month: month,
+        weekday: weekday,
+        day: now.getDate(),
+        hour: now.getHours() > 9 ? now.getHours() : `0${now.getHours()}`,
+        minutes: now.getMinutes() > 9 ? now.getMinutes() : `0${now.getMinutes()}`,
+        seconds: now.getSeconds() > 9 ? now.getSeconds() : `0${now.getSeconds()}`,
+    };
+}
+
+function displayCurrentTime() {
+    const timeInfoHTML = document.querySelector('#time-info');
+    timeData = getCurrentTime();
+    timeInfoHTML.innerHTML = `
+        <span>${timeData.hour}</span>:<span>${timeData.minutes}</span>
+    `;
+}
+
+function displayCurrentDate() {
+    const dateInfoHTML = document.querySelector('#date-info');
+    timeData = getCurrentTime();
+    dateInfoHTML.innerHTML = `
+        <span>${timeData.weekday}</span>, <span>${timeData.day}</span> de <span>${timeData.month}</span>
+    `;
+}
+
+function updateDisplays(displayToUpdate, timeInterval) {
+    setInterval(displayToUpdate, timeInterval);
 }
 
 function farenheitToCelcius(temperature) {
-    return ((temperature - 32) * (5/9));
+    return ((temperature - 32) * (5 / 9));
 }
 
 function kelvinToCelcius(temperature) {
-    return (temperature - 273,15);
+    console.log(temperature)
+    return (temperature - 273.15);
 }
